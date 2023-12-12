1b0c9fde6ada5365419d154ac3e7c956d843535b
Kevin Talbert <ktalbert@cloudera.com>
2023-12-12 13:04:32 -0500

    added amp script

--- 2_app-run-chroma-server/launch-server.py
+++ 2_app-run-chroma-server/launch-server.py
@@ -1,4 +1,5 @@
 import subprocess
 import os
 
+## Use this script to RUN Chroma hosted
 print(subprocess.run(["chroma run --path /home/cdsw/chroma-data --port " + str(os.environ['CDSW_APP_PORT'])], shell=True))
